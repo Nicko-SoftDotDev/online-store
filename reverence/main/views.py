fb5ef6a0015503bb5441cb6cbe4dbf4c813ffdfb
Nickolay Iliev <nickilev6@gmail.com>
2025-07-24 15:02:18 +0300

    solved the problem

--- reverence/main/views.py
+++ reverence/main/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView, DetailView
-from ,models import ClothingItem, Category, Size
+from .models import ClothingItem, Category, Size, \
+    ClothingItemSize
 from django.db.models import Q
+
 
 class CatalogView(ListView):
     model = ClothingItem
@@ -14,38 +16,46 @@ class CatalogView(ListView):
         size_names = self.request.GET.getlist('size')
         min_price = self.request.GET.get('min_price')
         max_price = self.request.GET.get('max_price')
+        search_query = self.request.GET.get('q')
 
-
-        if category_slugs:
+        if category_slugs: 
             queryset = queryset.filter(category__slug__in=category_slugs)
 
         if size_names:
             queryset = queryset.filter(
-                Q(sizes__name__in=size_names) & Q(size__clothingitemsize_availabel =True)
-            )
+                Q(sizes__name__in=size_names) & Q(sizes__clothingitemsize__available=True)
+            ).distinct()
+        
         if min_price:
-            queryset = queryset.filter(prince__gte=min_price)
+            queryset = queryset.filter(price__gte=min_price)
+
         if max_price:
-            queryset = queryset.filter(prince__lte=max_price)
+            queryset = queryset.filter(price__lte=max_price)
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) |
+                Q(description__icontains=search_query)
+            ).distinct()
 
         return queryset
+    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
-        context['size'] = Size.object.all()
-        context['selected_categories']  = self.request.GET.getlist('category')
-        context['selected_sizes']  = self.request.GET.getlist('size')
-        context['min_price'] = self.reqeuest.GET.get('min_price', ' ')
-        context['max_price'] = self.reqeuest.GET.get('max_price', ' ')
+        context['sizes'] = Size.objects.all()
+        context['selected_categories'] = self.request.GET.getlist('category')
+        context['selected_sizes'] = self.request.GET.getlist('size')
+        context['min_price'] = self.request.GET.get('min_price', '')
+        context['max_price'] = self.request.GET.get('max_price', '')
 
         return context
+    
 
-class ClothingItemDetailView(DetailView)
-      model = ClothingItem
-      template_name = 'main/product/detail.html'
-      context_object_name = 'clothind_item'
-      slug_field = 'slug'
-      slug_url_kwarg = 'slug'
-
-
-
+class ClothingItemDetailView(DetailView):
+    model = ClothingItem
+    template_name = 'main/product/detail.html'
+    context_object_name = 'clothing_item'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
